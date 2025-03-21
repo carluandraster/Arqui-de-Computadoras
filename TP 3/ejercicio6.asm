c9cd4d377210d4a182ff0bd3e5d3f542c8c91d1f
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-03-21 19:35:36 -0300

    Add files via upload

--- TP 3/ejercicio6.asm
+++ TP 3/ejercicio6.asm
@@ -11,8 +11,41 @@
         MOV EAX, 0; EAX: cociente
         MOV AC, EBX; AC: resto
         CMP EBX,0
-        MOV EFX, CC
-        CMP ECX, 0
-        XOR CC, EFX
-        JP POS
-        JN NEG
+        JN COND2
+        JZ FIN
+:COND1  CMP ECX, 0
+        JP CASO1
+        JN CASO2
+:COND2  CMP ECX, 0
+        JP CASO3
+        JN CASO4
+:CASO1  CMP AC, ECX
+        JN FIN
+        ADD EAX, 1
+        SUB AC, ECX
+        JMP CASO1
+:CASO2  MUL ECX, -1
+:OTRO2  CMP AC, ECX
+        JN FIN
+        SUB EAX, 1
+        SUB AC, ECX
+        JMP OTRO2
+:CASO3  CMP AC, 0
+        JNN FIN
+        SUB EAX, 1
+        ADD AC, ECX
+        JMP CASO3
+:CASO4  MUL AC,-1
+        MUL ECX, -1
+        JMP CASO1
+:FIN    MOV [1], EAX; Cociente
+        MOV [2], AC; Resto
+        MOV EDX, 0
+        ADD EDX, 1
+        MOV CH, 4
+        MOV CL, 1
+        MOV AL, 0x01
+        SYS 2
+        ADD EDX, 1
+        SYS 2
+        STOP
