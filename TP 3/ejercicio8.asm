16e85f14e089c933afb78a96e1c9d6886af1ba89
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-03-22 13:12:40 -0300

    Add files via upload

---
+++ TP 3/ejercicio8.asm
@@ -0,0 +1,23 @@
+OTRO:   MOV [1], 0; Contador de 1s
+        MOV EDX, DS
+        MOV CH, 4
+        MOV CL, 1
+        MOV AL, 0x01
+        SYS 1
+        CMP [EDX], 0
+        JN FIN
+ITER:   CMP [EDX], 0
+        JZ ESCRIB
+        MOV EFX, [EDX]
+        AND EFX, 1
+        ADD [1], EFX
+        SHR [EDX], 1
+        JMP ITER
+ESCRIB: MOV EDX, DS
+        ADD EDX, 1
+        MOV CH, 4
+        MOV CL, 1
+        MOV AL, 0x01
+        SYS 2
+        JMP OTRO
+:FIN    STOP
