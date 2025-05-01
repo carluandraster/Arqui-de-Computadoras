a1f198d165566993f353175512099e02edbe4515
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-04-30 22:07:34 -0300

    Add files via upload

--- TP 5/Ejercicio 1/recursiva.asm
+++ TP 5/Ejercicio 1/recursiva.asm
@@ -1,7 +1,8 @@
 ; DOCUMENTACIÓN del main
 ; [0]: base de la potencia
 ; [4]: exponente
-; [8]: resultado
+; [8]: resultado para mostrar por pantalla
+; EAX: registro reservado para que la función potencia devuelva su resultado
 
 TEXTO EQU "Ingrese un par de numeros: "
 
@@ -31,6 +32,7 @@ SIGUE0:     ; Llamar a la función
             PUSH [DS + 4]
             CALL potencia
             ADD SP, 4
+            MOV [8], EAX
         ; Mostrar resultado
             MOV EDX, DS
             ADD EDX, 8
@@ -43,7 +45,47 @@ SIGUE0:     ; Llamar a la función
 ; DOCUMENTACIÓN de la función potencia
 ; [BP + 12]: base
 ; [BP + 8]: exponente
-; [BP - 4]: variable de control para ciclo for
+; EAX: registro reservado para el retorno
+; EBX: registro auxiliar
             ; Prólogo
 potencia:       PUSH BP
                 MOV BP, SP
+                PUSH EBX
+            ; if(b == 0)
+            CMP [BP + 8], 0
+            JNZ ELSE
+                ; return 1
+                    MOV EAX, 1
+                    JMP FIN_POTENCIA
+            ; else if (b<0)
+ELSE:       JP ELSE2
+                ; return 1/potencia(a,-b)
+                    PUSH [BP+12]
+                    ; EBX := -b
+                        MOV EBX, 0
+                        SUB EBX, [BP + 8]
+                    PUSH EBX
+                    CALL potencia
+                    ADD SP, 4
+                    ; EBX := 1/EAX
+                        MOV EBX, 1
+                        DIV EBX, EAX
+                    MOV EAX, EBX
+                    JMP FIN_POTENCIA
+                ; else return a*potencia/(a,b-1)
+                    ; EBX := b-1
+ELSE2:                  MOV EBX, [BP+8]
+                        SUB EBX, 1
+                    PUSH [BP + 12]
+                    PUSH EBX
+                    CALL potencia
+                    ADD SP, 4
+                    ; EBX := a*EAX
+                        MOV EBX, [BP+12]
+                        MUL EBX, EAX
+                    MOV EAX, EBX
+            ; Epílogo
+FIN_POTENCIA:   POP EBX
+                MOV SP, BP
+                POP BP
+                RET
