02532ced5eda8737791fbe7973f15e8fc9193649
carluandraster <99754800+carluandraster@users.noreply.github.com>
2025-05-05 13:35:00 -0300

    Add files via upload

---
+++ TP 5/ejercicio3.asm
@@ -0,0 +1,10 @@
+; Ejercicio 3
+
+; a- Realizar una subrutina para convertir un string, que representa un número entero (positivo o negativo)
+; en base 10, a un número
+
+; ASCII
+; signo menos: 45
+
+; DOCUMENTACIÓN
+; [BP+8]: puntero a string
